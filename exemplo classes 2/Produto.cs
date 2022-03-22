a13c3ed6473ce7085deab9b7d5885bc8f8736d76
Mikael Toledo <mikaeltoledo31@gmail.com>
2022-03-22 19:26:49 -0300

    programa pronto

---
+++ exemplo classes 2/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace exemplo_classes_2{
+    class Produto{
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque(){
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade){
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade){
+            Quantidade = Quantidade - quantidade;
+        } 
+
+
+        public override string ToString(){
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
